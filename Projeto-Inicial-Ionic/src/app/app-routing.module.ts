2223b2c7fc25496abe819b1877a13d6344250a45
Theus1234567 <matheusgoncalvesoliveira47@gmail.com>
2024-10-03 16:30:02 -0300

    first-commit

--- Projeto-Inicial-Ionic/src/app/app-routing.module.ts
+++ Projeto-Inicial-Ionic/src/app/app-routing.module.ts
@@ -9,7 +9,28 @@ const routes: Routes = [
   {
     path: 'home',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  }
+  },
+  {
+    path: 'tab1',
+    loadChildren: () => import('./tab1/tab1.module').then( m => m.Tab1PageModule)
+  },
+  {
+    path: 'tab2',
+    loadChildren: () => import('./tab2/tab2.module').then( m => m.Tab2PageModule)
+  },
+  {
+    path: 'tab3',
+    loadChildren: () => import('./tab3/tab3.module').then( m => m.Tab3PageModule)
+  },
+  {
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
+  },
+  {
+    path: 'tab5',
+    loadChildren: () => import('./tab5/tab5.module').then( m => m.Tab5PageModule)
+  },
+  
 ];
 @NgModule({
   imports: [
